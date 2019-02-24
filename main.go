1ece788ecb1c7ad39e82c5069989d67057284cc9
isratmir <isratmir@yandex.ru>
2019-02-24 22:26:42 +0600

    Routes added

--- main.go
+++ main.go
@@ -8,23 +8,40 @@ import (
 	"github.com/labstack/echo"
 )
 
-var conn *sqlx.DB
-
-type Question struct {
-	id      int    `db:"id"`
-	Title   string `db:"title"`
-	Content string `db:"content"`
+type api struct {
+	DB *sqlx.DB
 }
 
-func insertQuestion(c echo.Context) error {
+type question struct {
+	Id      int    `db:"id" json:"id"`
+	Title   string `db:"title" json:"title"`
+	Content string `db:"content" json:"content"`
+}
+
+func newAPI() (*api, error) {
+	api := &api{}
 	conn, err := sqlx.Connect("mysql", "root:root@tcp(db:3306)/itchygo")
 	if err != nil {
 		panic(err)
 	}
+	api.DB = conn
+	return api, nil
+}
+
+func (api *api) listQuestions(c echo.Context) error {
+	qstns := []question{}
+	err := api.DB.Select(&qstns, "SELECT * FROM questions")
+	if err != nil {
+		panic(err)
+	}
+
+	return c.JSON(http.StatusOK, qstns)
+}
+
+func (api *api) insertQuestion(c echo.Context) error {
 	title := c.FormValue("title")
 	content := c.FormValue("content")
-	// insert := `INSERT INTO questions (title, content) VALUES (?, ?)`
-	res, err := conn.Exec(
+	res, err := api.DB.Exec(
 		`INSERT INTO questions (title, content) VALUES (?, ?)`,
 		title,
 		content,
@@ -39,8 +56,9 @@ func insertQuestion(c echo.Context) error {
 }
 
 func main() {
-
+	api, _ := newAPI()
 	e := echo.New()
-	e.POST("/questions", insertQuestion)
+	e.GET("/questions", api.listQuestions)
+	e.POST("/questions", api.insertQuestion)
 	e.Logger.Fatal(e.Start(":8080"))
 }
